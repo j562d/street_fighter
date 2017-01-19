6b7ce8edf22230cf24f533897ec1722cea4309f3
Jimmy Dong <jndong88@gmail.com>
2017-01-18 22:24:29 -0800

    changed image paths to urls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,25 +4,44 @@ module ApplicationHelper
 
 def character_img(e)
   if e.character == "Ryu"
-    return raw("<img class='style_image' src='assets/ryu_avatar.png'>")
+    return raw("<img class='style_image' src='http://i.imgur.com/mpa86wq.png'>")
   elsif e.character == "Ken"
-    return raw("<img class='style_image' src='assets/ken_avatar.jpg'>")
+    return raw("<img class='style_image' src='http://i.imgur.com/dsJ7mIp.jpg'>")
   elsif e.character == "Zangief"
-    return raw("<img class='style_image' src='assets/zangief_avatar.png'>")
+    return raw("<img class='style_image' src='http://i.imgur.com/8ExGmR9.png'>")
   elsif e.character == "M. Bison"
-    return raw("<img class='style_image' src='assets/mbison_avatar.png'>")
+    return raw("<img class='style_image' src='http://i.imgur.com/pARZgyp.png'>")
   elsif e.character == "Chun-Li"
-    return raw("<img class='style_image' src='assets/chunli_avatar.jpg'>")
+    return raw("<img class='style_image' src='http://i.imgur.com/DLhk1Mp.jpg'>")
   elsif e.character == "Dhalsim"
-    return raw("<img class='style_image' src='assets/dhalsim_avatar.png'>")
+    return raw("<img class='style_image' src='http://i.imgur.com/4NiwfWS.png'>")
   elsif e.character == "Vega"
-    return raw("<img class='style_image' src='assets/vega_avatar.jpg'>")
+    return raw("<img class='style_image' src='http://i.imgur.com/a2jKtH3.jpg'>")
   elsif e.character == "Nash"
-    return raw("<img class='style_image' src='assets/nash_avatar.jpg'>")
+    return raw("<img class='style_image' src='http://i.imgur.com/3fMeEou.jpg'>")
   end
 end
 
 
+def character_img_fav(d)
+  if d.combo.character == "Ryu"
+    return raw("<img class='style_image' src='http://i.imgur.com/mpa86wq.png'>")
+  elsif d.combo.character == "Ken"
+    return raw("<img class='style_image' src='http://i.imgur.com/dsJ7mIp.jpg'>")
+  elsif d.combo.character == "Zangief"
+    return raw("<img class='style_image' src='http://i.imgur.com/8ExGmR9.png'>")
+  elsif d.combo.character == "M. Bison"
+    return raw("<img class='style_image' src='http://i.imgur.com/pARZgyp.png'>")
+  elsif d.combo.character == "Chun-Li"
+    return raw("<img class='style_image' src='http://i.imgur.com/DLhk1Mp.jpg'>")
+  elsif d.combo.character == "Dhalsim"
+    return raw("<img class='style_image' src='http://i.imgur.com/4NiwfWS.png'>")
+  elsif d.combo.character == "Vega"
+    return raw("<img class='style_image' src='http://i.imgur.com/a2jKtH3.jpg'>")
+  elsif d.combo.character == "Nash"
+    return raw("<img class='style_image' src='http://i.imgur.com/3fMeEou.jpg'>")
+  end
+end
 
 
 def list
