3f10e5db03e7af9bd0866b5ad31451b3c037d28f
Jimmy Dong <jndong88@gmail.com>
2017-01-18 22:41:36 -0800

    added all images for all characters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,30 @@ def character_img(e)
     return raw("<img class='style_image' src='http://i.imgur.com/a2jKtH3.jpg'>")
   elsif e.character == "Nash"
     return raw("<img class='style_image' src='http://i.imgur.com/3fMeEou.jpg'>")
+  elsif e.character == "FANG"
+    return raw("<img class='style_image' src='http://i.imgur.com/OCWJKKs.jpg'>")
+  elsif e.character == "Alex"
+    return raw("<img class='style_image' src='http://i.imgur.com/6vgk495.png'>")
+  elsif e.character == "R. Mika"
+    return raw("<img class='style_image' src='http://i.imgur.com/AIFuoVg.png'>")
+  elsif e.character == "Rashid"
+    return raw("<img class='style_image' src='http://i.imgur.com/kVjb2PA.png'>")
+  elsif e.character == "Necalli"
+    return raw("<img class='style_image' src='http://i.imgur.com/X7NXSsH.png'>")
+  elsif e.character == "Laura"
+    return raw("<img class='style_image' src='http://i.imgur.com/n8kfQqR.jpg'>")
+  elsif e.character == "Guile"
+    return raw("<img class='style_image' src='http://i.imgur.com/lXInTOj.jpg'>")
+  elsif e.character == "Balrog"
+    return raw("<img class='style_image' src='http://i.imgur.com/Bm1QNZV.png'>")
+  elsif e.character == "Karin"
+    return raw("<img class='style_image' src='http://i.imgur.com/0TVnZvm.jpg'>")
+  elsif e.character == "Ibuki"
+    return raw("<img class='style_image' src='http://i.imgur.com/XHlL0Wo.jpg'>")
+  elsif e.character == "Birdie"
+    return raw("<img class='style_image' src='http://i.imgur.com/sAXOIUi.jpg'>")
+  elsif e.character == "Cammy"
+    return raw("<img class='style_image' src='http://i.imgur.com/nbkpjAf.jpg'>")
   end
 end
 
@@ -40,12 +64,36 @@ def character_img_fav(d)
     return raw("<img class='style_image' src='http://i.imgur.com/a2jKtH3.jpg'>")
   elsif d.combo.character == "Nash"
     return raw("<img class='style_image' src='http://i.imgur.com/3fMeEou.jpg'>")
+  elsif d.combo.character == "FANG"
+    return raw("<img class='style_image' src='http://i.imgur.com/OCWJKKs.jpg'>")
+  elsif d.combo.character == "Alex"
+    return raw("<img class='style_image' src='http://i.imgur.com/6vgk495.png'>")
+  elsif d.combo.character == "R. Mika"
+    return raw("<img class='style_image' src='http://i.imgur.com/AIFuoVg.png'>")
+  elsif d.combo.character == "Rashid"
+    return raw("<img class='style_image' src='http://i.imgur.com/kVjb2PA.png'>")
+  elsif d.combo.character == "Necalli"
+    return raw("<img class='style_image' src='http://i.imgur.com/X7NXSsH.png'>")
+  elsif d.combo.character == "Laura"
+    return raw("<img class='style_image' src='http://i.imgur.com/n8kfQqR.jpg'>")
+  elsif d.combo.character == "Guile"
+    return raw("<img class='style_image' src='http://i.imgur.com/lXInTOj.jpg'>")
+  elsif d.combo.character == "Balrog"
+    return raw("<img class='style_image' src='http://i.imgur.com/Bm1QNZV.png'>")
+  elsif d.combo.character == "Karin"
+    return raw("<img class='style_image' src='http://i.imgur.com/0TVnZvm.jpg'>")
+  elsif d.combo.character == "Ibuki"
+    return raw("<img class='style_image' src='http://i.imgur.com/XHlL0Wo.jpg'>")
+  elsif d.combo.character == "Birdie"
+    return raw("<img class='style_image' src='http://i.imgur.com/sAXOIUi.jpg'>")
+  elsif d.combo.character == "Cammy"
+    return raw("<img class='style_image' src='http://i.imgur.com/nbkpjAf.jpg'>")
   end
 end
 
 
 def list
-  ['Ken', 'Ryu', 'Zangief', 'M. Bison', 'Chun-Li', 'Dhalsim', 'Vega', 'Nash']
+  ["Alex", "Balrog", "Birdie", "Cammy", "Chun-Li", "Dhalsim", "FANG", "Guile", "Ibuki", "Karin", "Ken", "Laura", "M. Bison", "Nash", "Necalli", "Rashid", "R. Mika", "Ryu", "Vega", "Zangief" ]
 end
 
 end
